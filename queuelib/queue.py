eb35e7f4966e96cf1b33bab72947d6f8800e1819
Alexis Okuwa <wojonstech@gmail.com>
2014-08-19 21:19:24 -0700

    removed debug lines

--- queuelib/queue.py
+++ queuelib/queue.py
@@ -53,10 +53,13 @@ class FifoDiskQueue(object):
         hpos += 1
         szhdr = struct.pack(self.szhdr_format, len(string))
         os.write(self.headf.fileno(), szhdr + string)
-        if self.headf.tell() >= self.chunksize:
+        print self.headf.tell()
+        print self.chunksize
+        if self.headf.tell() >= self.chunksize: #roll to next chunk
             hpos = 0
             hnum += 1
             self.headf.close()
+            print "rolling"
             self.headf = self._openchunk(hnum, 'ab+')
         self.info['size'] += 1
         self.info['head'] = [hnum, self.headf.tell()]
