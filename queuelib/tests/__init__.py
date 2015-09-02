89f6df01f1dc43b938243d9c3fa3aa75e5dddda2
Daniel Graña <dangra@gmail.com>
2015-09-02 11:23:35 -0300

    regroup test cases so it is easier to reuse by different implementations

--- queuelib/tests/__init__.py
+++ queuelib/tests/__init__.py
@@ -4,6 +4,7 @@ class QueuelibTestCase(unittest.TestCase):
 
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp(prefix="queuelib-tests-")
+        self.qpath = self.mktemp()
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
--- queuelib/tests/test_queue.py
+++ queuelib/tests/test_queue.py
@@ -1,17 +1,52 @@
-import os, glob, tempfile
+import os
+import glob
 
 from queuelib.queue import FifoMemoryQueue, LifoMemoryQueue, FifoDiskQueue, LifoDiskQueue
 from queuelib.tests import QueuelibTestCase
 
-class FifoMemoryQueueTest(QueuelibTestCase):
+
+class BaseQueueTest(object):
 
     def queue(self):
-        return FifoMemoryQueue()
+        return NotImplementedError()
 
     def test_empty(self):
         """Empty queue test"""
         q = self.queue()
         assert q.pop() is None
+
+    def test_single_pushpop(self):
+        q = self.queue()
+        q.push(b'a')
+        assert q.pop() == b'a'
+
+    def test_binary_element(self):
+        elem = (
+            b'\x80\x02}q\x01(U\x04bodyq\x02U\x00U\t_encodingq\x03U\x05utf-'
+            b'8q\x04U\x07cookiesq\x05}q\x06U\x04metaq\x07}q\x08U\x07header'
+            b'sq\t}U\x03urlq\nX\x15\x00\x00\x00file:///tmp/tmphDJYsgU\x0bd'
+            b'ont_filterq\x0b\x89U\x08priorityq\x0cK\x00U\x08callbackq\rNU'
+            b'\x06methodq\x0eU\x03GETq\x0fU\x07errbackq\x10Nu.'
+        )
+        q = self.queue()
+        q.push(elem)
+        assert q.pop() == elem
+
+    def test_len(self):
+        q = self.queue()
+        self.assertEqual(len(q), 0)
+        q.push(b'a')
+        self.assertEqual(len(q), 1)
+        q.push(b'b')
+        q.push(b'c')
+        self.assertEqual(len(q), 3)
+        q.pop()
+        q.pop()
+        q.pop()
+        self.assertEqual(len(q), 0)
+
+
+class FifoTestMixin(BaseQueueTest):
 
     def test_push_pop1(self):
         """Basic push/pop test"""
@@ -38,29 +73,8 @@ class FifoMemoryQueueTest(QueuelibTestCase):
         self.assertEqual(q.pop(), b'd')
         self.assertEqual(q.pop(), b'e')
 
-    def test_len(self):
-        q = self.queue()
-        self.assertEqual(len(q), 0)
-        q.push(b'a')
-        self.assertEqual(len(q), 1)
-        q.push(b'b')
-        q.push(b'c')
-        self.assertEqual(len(q), 3)
-        q.pop()
-        q.pop()
-        q.pop()
-        self.assertEqual(len(q), 0)
 
-
-class LifoMemoryQueueTest(QueuelibTestCase):
-
-    def queue(self):
-        return LifoMemoryQueue()
-
-    def test_empty(self):
-        """Empty queue test"""
-        q = self.queue()
-        assert q.pop() is None
+class LifoTestMixin(BaseQueueTest):
 
     def test_push_pop1(self):
         """Basic push/pop test"""
@@ -87,30 +101,10 @@ class LifoMemoryQueueTest(QueuelibTestCase):
         self.assertEqual(q.pop(), b'b')
         self.assertEqual(q.pop(), b'a')
 
-    def test_len(self):
-        q = self.queue()
-        self.assertEqual(len(q), 0)
-        q.push(b'a')
-        self.assertEqual(len(q), 1)
-        q.push(b'b')
-        q.push(b'c')
-        self.assertEqual(len(q), 3)
-        q.pop()
-        q.pop()
-        q.pop()
-        self.assertEqual(len(q), 0)
 
-
-class FifoDiskQueueTest(FifoMemoryQueueTest):
+class PersistentTestMixin(object):
 
     chunksize = 100000
-
-    def setUp(self):
-        FifoMemoryQueueTest.setUp(self)
-        self.qdir = self.mktemp()
-
-    def queue(self):
-        return FifoDiskQueue(self.qdir, chunksize=self.chunksize)
 
     def test_text_in_windows(self):
         e1 = b'\r\n'
@@ -128,20 +122,53 @@ class FifoDiskQueueTest(FifoMemoryQueueTest):
         q.push(b'b')
         q.push(b'c')
         q.push(b'd')
-        self.assertEqual(q.pop(), b'a')
-        self.assertEqual(q.pop(), b'b')
+        q.pop()
+        q.pop()
         q.close()
         del q
+
         q = self.queue()
         self.assertEqual(len(q), 2)
         q.push(b'e')
-        self.assertEqual(q.pop(), b'c')
-        self.assertEqual(q.pop(), b'd')
+        q.pop()
+        q.pop()
         q.close()
         del q
+
         q = self.queue()
-        self.assertEqual(q.pop(), b'e')
+        assert q.pop() is not None
         self.assertEqual(len(q), 0)
+
+    def test_cleanup(self):
+        """Test queue dir is removed if queue is empty"""
+        q = self.queue()
+        values = [b'0', b'1', b'2', b'3', b'4']
+        assert os.path.exists(self.qpath)
+        for x in values:
+            q.push(x)
+
+        for x in values:
+            q.pop()
+        q.close()
+        assert not os.path.exists(self.qpath)
+
+
+class FifoMemoryQueueTest(FifoTestMixin, QueuelibTestCase):
+
+    def queue(self):
+        return FifoMemoryQueue()
+
+
+class LifoMemoryQueueTest(LifoTestMixin, QueuelibTestCase):
+
+    def queue(self):
+        return LifoMemoryQueue()
+
+
+class FifoDiskQueueTest(FifoTestMixin, PersistentTestMixin, QueuelibTestCase):
+
+    def queue(self):
+        return FifoDiskQueue(self.qpath, chunksize=self.chunksize)
 
     def test_chunks(self):
         """Test chunks are created and removed"""
@@ -150,85 +177,35 @@ class FifoDiskQueueTest(FifoMemoryQueueTest):
         for x in values:
             q.push(x)
 
-        chunks = glob.glob(os.path.join(self.qdir, 'q*'))
+        chunks = glob.glob(os.path.join(self.qpath, 'q*'))
         self.assertEqual(len(chunks), 5 // self.chunksize + 1)
         for x in values:
             q.pop()
 
-        chunks = glob.glob(os.path.join(self.qdir, 'q*'))
+        chunks = glob.glob(os.path.join(self.qpath, 'q*'))
         self.assertEqual(len(chunks), 1)
-
-    def test_cleanup(self):
-        """Test queue dir is removed if queue is empty"""
-        q = self.queue()
-        values = [b'0', b'1', b'2', b'3', b'4']
-        assert os.path.exists(self.qdir)
-        for x in values:
-            q.push(x)
-
-        for x in values:
-            q.pop()
-        q.close()
-        assert not os.path.exists(self.qdir)
 
 
 class ChunkSize1FifoDiskQueueTest(FifoDiskQueueTest):
     chunksize = 1
 
+
 class ChunkSize2FifoDiskQueueTest(FifoDiskQueueTest):
     chunksize = 2
 
+
 class ChunkSize3FifoDiskQueueTest(FifoDiskQueueTest):
     chunksize = 3
+
 
 class ChunkSize4FifoDiskQueueTest(FifoDiskQueueTest):
     chunksize = 4
 
 
-class LifoDiskQueueTest(LifoMemoryQueueTest):
-
-    def setUp(self):
-        LifoMemoryQueueTest.setUp(self)
-        self.path = tempfile.mktemp()
+class LifoDiskQueueTest(LifoTestMixin, PersistentTestMixin, QueuelibTestCase):
 
     def queue(self):
-        return LifoDiskQueue(self.path)
-
-    def test_close_open(self):
-        """Test closing and re-opening keeps state"""
-        q = self.queue()
-        q.push(b'a')
-        q.push(b'b')
-        q.push(b'c')
-        q.push(b'd')
-        self.assertEqual(q.pop(), b'd')
-        self.assertEqual(q.pop(), b'c')
-        q.close()
-        del q
-        q = self.queue()
-        self.assertEqual(len(q), 2)
-        q.push(b'e')
-        self.assertEqual(q.pop(), b'e')
-        self.assertEqual(q.pop(), b'b')
-        q.close()
-        del q
-        q = self.queue()
-        self.assertEqual(q.pop(), b'a')
-        self.assertEqual(len(q), 0)
-
-    def test_cleanup(self):
-        """Test queue file is removed if queue is empty"""
-        q = self.queue()
-        values = [b'0', b'1', b'2', b'3', b'4']
-        assert os.path.exists(self.path)
-        for x in values:
-            q.push(x)
-
-        for x in values:
-            q.pop()
-
-        q.close()
-        assert not os.path.exists(self.path)
+        return LifoDiskQueue(self.qpath)
 
     def test_file_size_shrinks(self):
         """Test size of queue file shrinks when popping items"""
@@ -236,8 +213,12 @@ class LifoDiskQueueTest(LifoMemoryQueueTest):
         q.push(b'a')
         q.push(b'b')
         q.close()
-        size = os.path.getsize(self.path)
+        size = os.path.getsize(self.qpath)
         q = self.queue()
         q.pop()
         q.close()
-        assert os.path.getsize(self.path), size
+        assert os.path.getsize(self.qpath), size
+
+
+
+
