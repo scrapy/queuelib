ba79050ae548bfa0298a34441c4ea37f8d9e7fb5
Daniel Graña <dangra@gmail.com>
2015-09-09 15:58:07 -0300

    Bump version: 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='queuelib',
-    version='1.4.1',
+    version='1.4.2',
     license='BSD',
     description='Collection of persistent (disk-based) queues',
     long_description=open('README.rst').read(),
