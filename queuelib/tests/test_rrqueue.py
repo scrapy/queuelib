f3bd12b5a893247d9f83f259750ba34f3155cdf7
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-04-05 18:25:32 -0300

    [queuelib.rrqueue] peek

--- queuelib/tests/test_rrqueue.py
+++ queuelib/tests/test_rrqueue.py
@@ -52,29 +52,41 @@ class base:
 
 
 class FifoTestMixin:
-    def test_push_pop_key(self):
+    def test_push_pop_peek_key(self):
+        self.assertEqual(self.q.peek(), None)
         self.q.push(b'a', '1')
         self.q.push(b'b', '1')
         self.q.push(b'c', '2')
         self.q.push(b'd', '2')
+        self.assertEqual(self.q.peek(), b'a')
         self.assertEqual(self.q.pop(), b'a')
+        self.assertEqual(self.q.peek(), b'c')
         self.assertEqual(self.q.pop(), b'c')
+        self.assertEqual(self.q.peek(), b'b')
         self.assertEqual(self.q.pop(), b'b')
+        self.assertEqual(self.q.peek(), b'd')
         self.assertEqual(self.q.pop(), b'd')
+        self.assertEqual(self.q.peek(), None)
         self.assertEqual(self.q.pop(), None)
 
 
 class LifoTestMixin:
 
     def test_push_pop_key(self):
+        self.assertEqual(self.q.peek(), None)
         self.q.push(b'a', '1')
         self.q.push(b'b', '1')
         self.q.push(b'c', '2')
         self.q.push(b'd', '2')
+        self.assertEqual(self.q.peek(), b'b')
         self.assertEqual(self.q.pop(), b'b')
+        self.assertEqual(self.q.peek(), b'd')
         self.assertEqual(self.q.pop(), b'd')
+        self.assertEqual(self.q.peek(), b'a')
         self.assertEqual(self.q.pop(), b'a')
+        self.assertEqual(self.q.peek(), b'c')
         self.assertEqual(self.q.pop(), b'c')
+        self.assertEqual(self.q.peek(), None)
         self.assertEqual(self.q.pop(), None)
 
 
