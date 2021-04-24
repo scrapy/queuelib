093ba3005d0bd4e7843dcd816735ea24a8fd4d28
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-04-24 19:11:57 -0300

    Move __version__ attribute to the top of the file

--- queuelib/__init__.py
+++ queuelib/__init__.py
@@ -1,5 +1,5 @@
+__version__ = "1.6.1"
+
 from queuelib.queue import FifoDiskQueue, LifoDiskQueue
 from queuelib.pqueue import PriorityQueue
 from queuelib.rrqueue import RoundRobinQueue
-
-__version__ = "1.6.1"
